2a1ab7aa25e3620f4f5c1621f1fc584684d26043
rafael <rafaelmagalhaes1@live.com>
2024-01-23 10:31:43 -0300

    feat: create app.js and server.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// src/app.js
+
+const express = require('express');
+
+const employee = require('./controllers/employee.controller');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.status(200).json({ message: 'Server is running!' });
+});
+
+app.get('/employees', employee.getAll);
+
+module.exports = app;
